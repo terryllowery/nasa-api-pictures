2aa43c4a44380210508c57cc8cb119a5c760c05f
Terry Lowery <tlowe33@wgu.edu>
2020-08-06 08:02:41 -0500

    Added save to localstorage

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
     resultsArray.forEach((result) => {
@@ -39,6 +40,7 @@ function updateDOM() {
         const saveText = document.createElement('p');
         saveText.classList.add('clickable');
         saveText.textContent = 'Add To Favorties';
+        saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
         // Card Text
         const cardText = document.createElement('p');
         cardText.textContent = result.explanation;
@@ -72,6 +74,25 @@ async function getNasaPictures() {
     } catch (error) {
         // Catch error here
     }
+}
+
+// Add result to Favorites
+function saveFavorite(itemUrl) {
+    // Loop through results array to select favorites
+    resultsArray.forEach((item) => {
+        if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+            favorites[itemUrl] = item;
+            console.log(JSON.stringify(favorites));
+            // Show save Conformatioin for 2 seconds
+            saveConfirmed.hidden = false;
+            setTimeout(() => {
+                saveConfirmed.hidden = true;
+            }, 2000);
+            // Set favorites in localstorage
+            // TODO: Figure out why page refresh then setItem clears localStorage
+            localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+        }
+    })
 }
 
 // On Load
