b15e8a3ea86e79c0de1b3ea8ffb9c37d489feb1d
Terry Lowery <tlowe33@wgu.edu>
2020-08-06 22:13:24 -0500

    Added showing favorites only

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-    resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+    console.log('Current Array', page, currentArray);
+    currentArray.forEach((result) => {
         // Card Container
         const card = document.createElement('div');
         card.classList.add('card');
@@ -64,13 +66,24 @@ function updateDOM() {
     });
 }
 
+function updateDOM(page) {
+    
+   if(localStorage.getItem('nasaFavorites')) {
+    favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+    console.log("********favorites from local storage:")
+    console.log(favorites);
+
+}
+   createDOMNodes(page);
+}
+
 // Get 10 images from NASA api
 async function getNasaPictures() {
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
-        console.log(resultsArray);
-        updateDOM();
+        // TODO: remove extra console.log
+            updateDOM('favorites');
     } catch (error) {
         // Catch error here
     }
@@ -82,7 +95,7 @@ function saveFavorite(itemUrl) {
     resultsArray.forEach((item) => {
         if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
             favorites[itemUrl] = item;
-            console.log(JSON.stringify(favorites));
+            // console.log(JSON.stringify(favorites));
             // Show save Conformatioin for 2 seconds
             saveConfirmed.hidden = false;
             setTimeout(() => {
